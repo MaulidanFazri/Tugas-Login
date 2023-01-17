e0bdd7bdff15a6c12f8ae4b97a7cbb33fd513880
Maulidan Fazri <122434808+MaulidanFazri@users.noreply.github.com>
2023-01-17 21:25:21 +0700

    Add files via upload

---
+++ biodata.h
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <iostream.h>
+biodata(char jud[50]=""){
+   cout<<"_______________________________________________________________"<<endl;
+	cout<<"                           "<<jud<<endl;
+  	cout<<"---------------------------------------------------------------"<<endl;
+   if(strcmp(jud,"BIODATA")==0){
+    	cout<<"Nama		: Maulidan Fazri"<<endl;
+   	cout<<"Kelas		: 10 PPLG"<<endl;
+   	cout<<"Username	: maulidan"<<endl;
+   	cout<<"Password	: fazri"<<endl;
+   cout<<"---------------------------------------------------------------"<<endl;
+   }
+}
